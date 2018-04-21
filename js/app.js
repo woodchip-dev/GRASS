3e95b6568937634bf6f923ed4ae74cbc865cd6a6
amitchell <amitchell@alaska.edu>
2018-04-20 20:33:59 -0800

    fleshed out some of the functions, no calls to server yet

--- js/app.js
+++ js/app.js
@@ -1,76 +1,188 @@
 $(document).foundation()
-jQuery(document).ready(function(){
-    // This button will increment the value
-    $('[data-quantity="plus"]').click(function(e){ // Stop acting like a button
-        e.preventDefault(); // Get the field name
-        fieldName = $(this).attr('data-field'); // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val()); // If is not undefined
-        if (!isNaN(currentVal)) { // Increment
+
+jQuery(document).ready(function()
+{
+    $('[data-quantity="plus"]').click(function(e)
+    {
+        e.preventDefault();
+        fieldName = $(this).attr('data-field');
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        if (!isNaN(currentVal))
+        {
             $('input[name='+fieldName+']').val(currentVal + 1);
-        } else { // Otherwise put a 0 there
+        }
+        else
+        {
             $('input[name='+fieldName+']').val(0);
         }
     });
-    // This button will decrement the value till 0
-    $('[data-quantity="minus"]').click(function(e) { // Stop acting like a button
-        e.preventDefault(); // Get the field name
-        fieldName = $(this).attr('data-field'); // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val()); // If it isn't undefined or its greater than 0
-        if (!isNaN(currentVal) && currentVal > 0) { // Decrement one
+
+    $('[data-quantity="minus"]').click(function(e)
+    {
+        e.preventDefault();
+        fieldName = $(this).attr('data-field');
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        if (!isNaN(currentVal) && currentVal > 0)
+        {
             $('input[name='+fieldName+']').val(currentVal - 1);
-        } else {  // Otherwise put a 0 there
+        }
+        else
+        {
             $('input[name='+fieldName+']').val(0);
         }
     });
 });
 
-function on_bad_bid() {
+function on_bad_bid()
+{
     document.getElementById("bad-bid").style.display = "block";
 }
 
-function off_bad_bid() {
+function off_bad_bid()
+{
     document.getElementById("bad-bid").style.display = "none";
 }
 
-function on_attempt_login() {
+function on_good_bid()
+{
+    document.getElementById("good-bid").style.display = "block";
+}
+
+function off_good_bid()
+{
+    document.getElementById("good-bid").style.display = "none";
+}
+
+function on_attempt_login()
+{
     document.getElementById("i-am-root").style.display = "block";
 }
 
-function off_attempt_login() {
+function off_attempt_login()
+{
     document.getElementById("i-am-root").style.display = "none";
 }
 
-function attempt_logout() {
-    // return to main page, end session somehow
+function login()
+{
+    var log_form = document.forms["pass"].elements;
+
+    var password = log_form["word"].value
+    
+    if(password = "")
+    {
+        on_bad_bid();
+    }
+    else
+    {
+        // send to server for verification!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // get something back from server to say password was wrong
+        if(false)
+        {
+            on_bad_bid();
+        }
+    }
 }
 
-function make_bid() {
-    on_bad_bid();
-    // add bid and iz number (if applicable) to database
+function attempt_logout()
+{
+    // return to main page, end session somehow!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
-function show_bids() {
-    // require IZN, otherwise VERBOTEN
-    on_bad_bid();
-    document.getElementById("dispBala").style.display = "initial";
-    document.getElementById("dispBids").style.display = "initial";
-    // display all bids for a particular IZN
+function make_bid()
+{
+    var bid_form = document.forms["create_bid"].elements;
+
+    var iz_number = bid_form["iz-number"].value;
+    var root = bid_form["root"].value;
+    var num_roots = bid_form["num-roots"].value;
+    var bid_amt = bid_form["bid-amt"].value;
+
+    if(bid_amt > 100 || bid_amt < 1 || num_roots < 1 || iz_number == "" || iz_number.length < 6)
+    {
+        on_bad_bid();
+    }
+    else
+    {
+        on_good_bid();
+        // send to server!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
 }
 
-function reset_view() {
+function show_bids()
+{
+    var disp_bids = document.forms["iz-man"].elements;
+
+    var iz_number = disp_bids["iz-number"].value
+
+    if(iz_number == "" || iz_number.length < 6)
+    {
+        on_bad_bid();
+    }
+    else
+    {
+        // retrieve info from server for display, somehow display
+    // display all bids for a particular IZN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        document.getElementById("dispBala").style.display = "initial";
+        document.getElementById("dispBids").style.display = "initial";
+    }
+}
+
+function reset_view()
+{
+    // show no bid info
+    // somehow actually clear the data
     document.getElementById("dispBala").style.display = "none";
     document.getElementById("dispBids").style.display = "none";
-    // show no bid info
 }
 
-function change_bid() {
-    // change bid
+function change_bid()
+{
+    // change bid!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
-function delete_bid() {
-    // delete bid
+function delete_bid()
+{
+    // delete bid!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
-function update_roots() {
+function update_roots()
+{
+    var updates = document.forms["updater"].elements;
+
+    var add = updates["add-root"].value;
+    var del = updates["del-root"].value;
+
+    if(add != "" && del != "")
+    {
+        // send add & del to server
+        on_good_bid();
+    }
+    else if(add != "" && del == "")
+    {
+        // send add to server
+        on_good_bid();
+    }
+    else if(add == "" && del != "")
+    {
+        // send del to server
+        on_good_bid();
+    }
+    else
+    {
+        on_bad_bid();
+    }
     // add and/or delete root
+    show_roots();
+}
+
+function show_roots()
+{
+    // get roots from server & display!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+}
+
+function show_root_bids()
+{
+    show_roots();
+    // get bid data from server & display!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
